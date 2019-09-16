72a886edb30a806d8d9c24de70534bf2b0e22eca
caoxy <sxinyus@126.com>
2019-09-16 14:19:44 +0800

    添加model部分接口

--- Spectrogram/Spectrogram.h
+++ Spectrogram/Spectrogram.h
@@ -2,11 +2,13 @@
 #define SPECTROGRAM_H
 
 #include <QGraphicsView>
+#include "SpectrogramModel.h"
 
 class Spectrogram : public QGraphicsView
 {
 public:
     Spectrogram();
+    void setModel(SpectrogramModel* pModel);    //设置数据模型
 
 private:
     QGraphicsScene* m_pSence;
--- Spectrogram/SpectrogramModel.h
+++ Spectrogram/SpectrogramModel.h
@@ -1,11 +1,40 @@
 #ifndef SPECTROGRAMMODEL_H
 #define SPECTROGRAMMODEL_H
 
+#include <QObject>
+#include <QVector>
+#include <QMap>
+#include <QColor>
+#include <QRectF>
 
-class SpectrogramModel
+typedef struct _SLineData
 {
+    QVector<float> vecData;
+    QColor lineColor;
+}
+SLineData;
+
+class SpectrogramModel : public QObject
+{
+    Q_OBJECT
 public:
     SpectrogramModel();
+    //get/set数据范围
+    QRectF getDataRange() { return m_dataRange; }
+    void setDataRange(QRectF dataRange) { m_dataRange = dataRange; }
+    void setXDataRange(qreal minX, qreal maxX);
+    void setYDataRnage(qreal minY, qreal maxY);
+    //get/set显示范围
+    QRectF getShowRange() { return m_showRange; }
+    void setShowRange(QRectF showRange) { m_showRange = showRange; }
+    void setXShowRange(qreal minX, qreal maxX);
+    void setYShowRange(qreal minY, qreal maxY);
+    int setData(QString, SLineData);
+
+private:
+    QMap<QString, SLineData> m_mapLineData;
+    QRectF m_dataRange; //存储数据范围
+    QRectF m_showRange; //存储显示范围, 显示范围不可大于数据范围
 };
 
 #endif // SPECTROGRAMMODEL_H
